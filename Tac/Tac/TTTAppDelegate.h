ba437ecc23bcc20498b4c6020850d609f07e2810
jhend11 <jhend11@uw.edu>
2014-07-29 09:32:05 -0400

    Just started

---
+++ Tac/Tac/TTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTTAppDelegate.h
+//  Tac
+//
+//  Created by JOSH HENDERSHOT on 7/29/14.
+//  Copyright (c) 2014 Joshua Hendershot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
